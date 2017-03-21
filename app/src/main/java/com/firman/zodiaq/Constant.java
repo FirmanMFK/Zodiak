82ea70f09de56c10d766f7984f3515471b70db99
FirmanMFK <firman.mfk@gmail.com>
2017-03-22 00:09:48 +0700

    commit awesome project

---
+++ app/src/main/java/com/firman/zodiaq/Constant.java
@@ -0,0 +1,15 @@
+package com.firman.zodiaq;
+
+/**
+ * Created by Firman on 3/21/2017.
+ */
+
+public class Constant {
+
+    public static final String ALAMAT_SERVER = "http://ibacor.com";
+
+    public static final int KODE_BUS_HAPUS_SEMUA = 1;
+    public static final int KODE_BUS_INVALIDASI_MENU_HAPUS = 2;
+
+
+}
