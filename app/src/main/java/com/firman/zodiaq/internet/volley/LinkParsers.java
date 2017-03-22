d09c94300889738b5165d81729669866327aa9c8
FirmanMFK <firman.mfk@gmail.com>
2017-03-22 14:09:39 +0700

    OkHttp commit

--- app/src/main/java/com/firman/zodiaq/internet/volley/LinkParsers.java
+++ app/src/main/java/com/firman/zodiaq/internet/volley/LinkParsers.java
@@ -1,8 +1,47 @@
 package com.firman.zodiaq.internet.volley;
 
+import java.net.URLEncoder;
+import java.util.Map;
+
 /**
  * Created by Firman on 3/22/2017.
  */
 
 public class LinkParsers {
+
+    public LinkParsers() {
+
+    }
+
+    //Ubah Parameters ke bentuk Link quey untuk request tipe get
+    public static String setParamsLinks(Map<String, String> mapParams) {
+
+        String urlLinks = "";
+        StringBuilder stringBuilder = new StringBuilder();
+
+        if (mapParams != null) {
+            for (String key : mapParams.keySet()) {
+                Object value = mapParams.get(key);
+                if (value != null) {
+                    try {
+
+                        String valuesLink = URLEncoder.encode(String.valueOf(value), "UTF-8");
+                        if (stringBuilder.length() > 0) {
+                            stringBuilder.append("&");
+                        }
+
+                        stringBuilder.append(key);
+                        stringBuilder.append("=");
+                        stringBuilder.append(valuesLink);
+
+                    } catch (Exception ex) {
+                        ex.printStackTrace();
+                    }
+                }
+            }
+
+            urlLinks = stringBuilder.toString();
+        }
+        return urlLinks;
+    }
 }
--- app/src/main/java/com/firman/zodiaq/internet/volley/OkHttpSingleton.java
+++ app/src/main/java/com/firman/zodiaq/internet/volley/OkHttpSingleton.java
@@ -1,8 +1,61 @@
 package com.firman.zodiaq.internet.volley;
 
+
+import java.io.IOException;
+import java.util.concurrent.TimeUnit;
+
+import okhttp3.Interceptor;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
+
 /**
  * Created by Firman on 3/22/2017.
  */
 
 public class OkHttpSingleton {
+
+    private OkHttpClient mOkHttpClient;
+
+    private static OkHttpSingleton ourInstance;
+
+    public static OkHttpSingleton getInstance() {
+        if (ourInstance == null) {
+            ourInstance = new OkHttpSingleton();
+        }
+        return ourInstance;
+    }
+
+    private OkHttpSingleton() {
+
+        OkHttpClient.Builder builderOkhttp = new OkHttpClient.Builder();
+        builderOkhttp.connectTimeout(30, TimeUnit.SECONDS);
+        builderOkhttp.readTimeout(50, TimeUnit.SECONDS);
+        builderOkhttp.writeTimeout(50, TimeUnit.SECONDS);
+        builderOkhttp.addInterceptor(getInterceptor());
+
+        mOkHttpClient = builderOkhttp.build();
+        setOkHttpClient(mOkHttpClient);
+    }
+
+    private Interceptor getInterceptor() {
+        return new Interceptor() {
+            @Override
+            public Response intercept(Chain chain) throws IOException {
+                Request newRequest = chain.request().newBuilder()
+                        .addHeader("Accept", "application/json")
+                        .build();
+
+                return chain.proceed(newRequest);
+            }
+        };
+    }
+
+    public OkHttpClient getOkHttpClient() {
+        return mOkHttpClient;
+    }
+
+    public void setOkHttpClient(OkHttpClient okHttpClient) {
+        mOkHttpClient = okHttpClient;
+    }
 }
